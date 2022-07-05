48c672dc59dbee0dcd53d713cf28cbbcf20d07d3
Luis Gabriel García Mingall <90941271+luisga88gm@users.noreply.github.com>
2022-07-05 00:53:09 -0300

    desafio 34

--- Desktop/CODER/30930-programación-backend/Clase 34 - OnCloudProyect/ecosystem.config.js
+++ Desktop/CODER/30930-programación-backend/Clase 34 - OnCloudProyect/ecosystem.config.js
@@ -1,51 +1,18 @@
-/**
- * https://pm2.keymetrics.io/docs/usage/application-declaration/
- */
- module.exports = {
+module.exports = {
     apps: [
         {
             name: 'app0',
             script: 'src/index.js',
             watch: true,
             autorestart: true,
-            args: '-p=8080',
-        },
-        {
-            name: 'app1',
-            script: 'src/index.js',
-            watch: true,
-            autorestart: true,
-            args: '-p=8081',
-        },
-        {
-            name: 'app2',
-            script: 'src/index.js',
-            watch: true,
-            autorestart: true,
-            args: '-p=8082',
-        },
-        {
-            name: 'app3',
-            script: 'src/index.js',
-            watch: true,
-            autorestart: true,
-            args: '-p=8083',
-        },{
-            name: 'app4',
-            script: 'src/index.js',
-            watch: true,
-            autorestart: true,
-            args: '-p=8084',
-        },{
-            name: 'app5',
-            script: 'src/index.js',
-            watch: true,
-            autorestart: true,
-            args: '-p=8085',
-        },
-        // {
-            // script: './service-worker/',
-            // watch: ['./service-worker'],
-        // },
-    ],
+            instances: 'max',
+            env:{
+                NODE_ENV: 'development',
+            },
+            en_production:{
+                NODE_ENV: 'production'
+            }
+        }
+
+    ]
 };
